46c77203c4e9745a3e3896d95c300ce7f0175ac4
quantum2511 <bhavya.chanana2021@vitstudent.ac.in>
2023-09-16 01:07:49 +0530

    added somewhat alternate approach

--- python/python/0217-contains-duplicate.py
+++ python/python/0217-contains-duplicate.py
@@ -7,4 +7,6 @@ def containsDuplicate(nums):
             return True
         hashset.add(n)
     return False
-      
+
+# u can also add all the values to hashset and compare the size of hashset and nums
+# as hashset doesnt allow duplicate values therefore if the size of hashset < size of nums: return true contains duplicate
