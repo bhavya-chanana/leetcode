9b98a275778bd688a772895d4dc7dbed29133da3
quantum2511 <bhavya.chanana2021@vitstudent.ac.in>
2023-09-16 00:43:02 +0530

    solved-0026-remove-duplicates-in-sorted-array

---
+++ python/python/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,12 @@
+def removeDuplicates(nums):
+    count = 0
+    for i in range(len(nums)):
+        if i<len(nums)-1 and nums[i] == nums[i+1]:
+            continue
+        nums[count] = nums[i]   #as the array is sorted and count = distinct values in array thus we can replace the no at count index in num array with the next distinct no
+        # print(nums)
+        count+=1
+    return count
+
+nums = [1,1,2,2,3,4,4,5]
+print(removeDuplicates(nums))
