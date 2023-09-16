d707007166970978451cc9aefdf98c3a04e5e57f
Bhavya Chanana <65809399+Quantum2511@users.noreply.github.com>
2023-09-17 01:14:08 +0530

    Time: 93 ms (86.66%), Space: 20.8 MB (99.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,7 +4,8 @@ class Solution:
         l = []
         for i in nums:
             d[i]=d.get(i, 0) + 1
-        sorted_d = sorted(d.items(), key=lambda x:x[1], reverse=True)
-        for i in range(k):
-            l.append(sorted_d[i][0])
+        m = sorted(d.values(), reverse=True)[k-1]
+        for key in d:
+            if d[key]>=m: 
+                l.append(key)
         return l
