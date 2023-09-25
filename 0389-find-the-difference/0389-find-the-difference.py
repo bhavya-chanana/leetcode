6e4f5edd4169ddee50c208f3a4fe5902e576bfeb
Bhavya Chanana <65809399+Quantum2511@users.noreply.github.com>
2023-09-25 23:40:54 +0530

    Time: 47 ms (24.67%), Space: 16.2 MB (76.19%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s = sorted(list(s))
+        t = sorted(list(t))
+        d = {}
+        for i in s:
+            d[i]=d.get(i, 0) + 1
+        for i in t:
+            if d.get(i):
+                d[i] = d[i] - 1
+            else:
+                return i
+            # d[i] = d.get(i, 0) - 1
+            # if d[i] == -1:
+            #     return i
+            
