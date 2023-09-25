d200d0b2f07826046f75ff9365f0f4ff8811a344
Bhavya Chanana <65809399+Quantum2511@users.noreply.github.com>
2023-09-25 23:58:42 +0530

    Time: 36 ms (85.36%), Space: 16.4 MB (43.09%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -4,10 +4,6 @@ class Solution:
         for i in s:
             d[i]=d.get(i, 0) + 1
         for i in t:
-            # if d.get(i):
-            #     d[i] = d[i] - 1
-            # else:
-            #     return i
             d[i] = d.get(i, 0) - 1
             if d[i] == -1:
                 return i
