b1f41d0212143abcacd92a162834a8a6ff98741a
Bhavya Chanana <65809399+Quantum2511@users.noreply.github.com>
2023-09-27 22:50:32 +0530

    Time: 103 ms (46.09%), Space: 22.2 MB (16.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = collections.defaultdict(list) #makes an new empty list when there key aint there rather than raising keyError
         for s in strs:
-            count = [0] * 26
+            count = [0] * 26    #array for storing count of a...z
 
             for c in s:
                 count[ord(c) - ord('a')] += 1
