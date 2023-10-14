93a9b8c74c1c17abeef9d60ad97f0c122a2370b5
Bhavya Chanana <65809399+Quantum2511@users.noreply.github.com>
2023-10-15 02:16:24 +0530

    added another soln

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,3 +1,5 @@
+### both soln have same time complexity --- O(m * n)
+#using string manipulation 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if needle == "":
@@ -5,4 +7,18 @@ class Solution:
         for i in range(len(haystack) - len(needle) + 1):
             if haystack[i: i + len(needle)] == needle:
                 return i 
+        return -1   
+
+
+# using nested for loops
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        for index in range(len(haystack) - len(needle) + 1):
+            for j in range(len(needle)):
+                if haystack[index + j] != needle[j]:
+                    break
+                if j == len(needle) - 1:
+                    return index
         return -1   
