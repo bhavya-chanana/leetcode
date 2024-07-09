97517db3906d98b03643c0fdab16a785bfcf48da
Bhavya Chanana <65809399+bhavya-chanana@users.noreply.github.com>
2024-07-09 16:06:11 +0530

    Time: 777 ms (5.04%), Space: 29.7 MB (57.21%) - LeetHub

---
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        l, r = 0, k - 1
+        maxSum = k * threshold
+        window = []
+        s = 0
+        count = 0
+        for r in range(len(arr)):
+            if len(window) == k:
+                
+                if s >= maxSum:
+                    count += 1
+                s = s - window[0]
+                window.pop(0)
+            
+            if len(window) < k:
+                s = s + arr[r]
+                window.append(arr[r])
+
+        s = sum(window)
+        if s >= maxSum:
+            count += 1
+
+        return count
+
+            
