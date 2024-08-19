278109c2e1fdf7724a4209b446323f0479ab6b54
Bhavya Chanana <65809399+bhavya-chanana@users.noreply.github.com>
2024-08-19 15:12:01 +0530

    Update 0543-diameter-of-binary-tree.py

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -26,39 +26,6 @@ class Solution:
         return self.res
 
 
-# ****[543. Diameter of Binary Tree](https://leetcode.com/problems/diameter-of-binary-tree/)****
-
-**notes:**
-
-```python
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        # global variable 
-        self.res = 0
-
-        # Returns height
-        def dfs(curr):
-            if not curr:
-                return 0
-            
-            left = dfs(curr.left)
-            right = dfs(curr.right)
-
-            self.res = max(self.res, left + right)
-
-            return 1 + max(left, right)
-        
-        dfs(root)
-
-        return self.res
-```
-
 # for not defining global variable at the start → make nonlocal res inside the dfs function
 ```python
 # for not defining global variable at the start 
