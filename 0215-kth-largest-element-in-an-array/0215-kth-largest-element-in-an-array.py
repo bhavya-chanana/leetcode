db649016243363bd3814b193f9f4cf160743565a
Bhavya Chanana <65809399+bhavya-chanana@users.noreply.github.com>
2024-09-22 01:54:43 +0530

    Update 0215-kth-largest-element-in-an-array.py

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,4 +1,21 @@
+## Soln 1 - using sorting - O(N) = N logN
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         nums.sort(reverse = True)
         return(nums[k - 1])
+
+## Soln 2 - using minHeap
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        minHeap = []
+        for num in  nums:
+            heappush(minHeap, num)
+            if len(minHeap) > k:
+                heappop(minHeap)
+            print(minHeap)
+
+        return abs(minHeap[0]) 
+
+## Soln 3 - using quickSelect algorithm - 
+# O(n) = N -> average case
+# O(n) = N^2 -> worst case
