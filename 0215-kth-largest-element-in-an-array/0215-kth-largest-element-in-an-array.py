60aa0336450897a5dc97fb8c06df1e7dff7a84da
Bhavya Chanana <65809399+bhavya-chanana@users.noreply.github.com>
2024-09-22 11:08:52 +0530

    Update 0215-kth-largest-element-in-an-array.py

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -19,3 +19,21 @@ class Solution:
 ## Soln 3 - using quickSelect algorithm - 
 # O(n) = N -> average case
 # O(n) = N^2 -> worst case
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        self.k = len(nums) - k
+        def quickSelect(left, right):
+            pivot, p = nums[right], left
+             
+            for i in range(left, right):
+                if nums[i] < pivot:
+                    nums[i], nums[p] = nums[p], nums[i]
+                    p += 1
+            
+            nums[p], nums[right] = nums[right], nums[p]
+
+            if p > self.k: return quickSelect(left, p - 1)
+            elif p < self.k: return quickSelect(p + 1, right)
+            else: return nums[p]
+
+        return quickSelect(0, len(nums) - 1)
